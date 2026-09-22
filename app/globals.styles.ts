58a974dce46f105572322fe7a6d8ccc9f1974b5c
CallumD21 <callum.dickinson176@gmail.com>
2026-09-22 22:37:16 +0100

    Create basic login form

--- app/globals.styles.ts
+++ app/globals.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Heading = styled.h1`
   font-size: 2rem;
   font-weight: bold;
+  margin-bottom: 3rem;
 `;
 
 export const Button = styled.button<{ $primary?: boolean }>`
@@ -13,4 +14,26 @@ export const Button = styled.button<{ $primary?: boolean }>`
   border: 2px solid palevioletred;
   border-radius: 3px;
   cursor: pointer;
+  margin-bottom: 1rem;
+`;
+
+export const Input = styled.input`
+  display: block;
+  width: 100%;
+  max-width: 300px;
+  border: 2px solid black;
+  padding: 0.25rem;
+  margin-bottom: 1rem;
+`;
+
+export const TextButton = styled.button`
+  text-decoration: underline;
+  cursor: pointer;
+ 
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+export const Form = styled.form`
 `;
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
-import { Button, Heading } from "./globals.styles";
+import { Button, Form, Heading, Input, TextButton } from "./globals.styles";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        <div>
           <Heading>
             Amber and Callum's Wedding Website
           </Heading>
+          <Form>
+            <Input placeholder="Username" type="text" />
+            <Input placeholder="Password" type="password" />
+            <Button type="submit">
+              Log in
+            </Button>
+          </Form>
+          <TextButton>
+            Create account
+          </TextButton>
         </div>
-          <Button>
-            Test
-          </Button>
       </main>
     </div>
   );
